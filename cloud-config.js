851f15646cf010429aa223e4ac160e29ab3b5988
Haruto Ishigaki <47k99ksf97@privaterelay.appleid.com>
2026-07-09 07:18:47 +0900

    Update GTJ_SUPABASE configuration

--- cloud-config.js
+++ cloud-config.js
@@ -1,1 +1,5 @@
-window.GTJ_SUPABASE = null;
+https://pbltaskmanager.47k99ksf97.workers.dev/
+window.GTJ_SUPABASE = {
+  url: "https://cwaoznqmtyvvlarnnovo.supabase.co",
+  anonKey: "sb_publishable_TvNG7Lsi06DdAxincrPzuA_RMM-myFd"
+};
