847a3380e002262b8b54aac48df4fda45e1a1a68
Haruto Ishigaki <47k99ksf97@privaterelay.appleid.com>
2026-07-09 07:35:07 +0900

    Update GTJ_AI_PROXY_URL in cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -1,5 +1,5 @@
-https://pbltaskmanager.47k99ksf97.workers.dev/
 window.GTJ_SUPABASE = {
   url: "https://cwaoznqmtyvvlarnnovo.supabase.co",
   anonKey: "sb_publishable_TvNG7Lsi06DdAxincrPzuA_RMM-myFd"
 };
+window.GTJ_AI_PROXY_URL = "https://pbltaskmanager.47k99ksf97.workers.dev";
